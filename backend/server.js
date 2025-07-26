3e4282ce5c412b0ec306e8f6451fa04534f83f71
Rakesh4440 <rakeshvarmaks999@gmail.com>
2025-07-26 23:17:45 +0530

    Added root test route

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,7 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is working ✅");
+});
